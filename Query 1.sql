35b342df315eff6d469b206a17b3b13857873b11
Paaathss <fathimanasrinek@gmail.com>
2025-01-25 12:46:07 +0530

    Add files via upload

---
+++ Query 1.sql
@@ -0,0 +1,23 @@
+-- use schema2 
+
+-- create table grp(salary int)
+
+-- insert into grp(salary) values(12000)
+-- select * from grp
+
+-- create table AnnualSalary(ID int, Name varchar(20), age int, Occupation varchar(20), Salary int) 
+
+-- insert into AnnualSalary(ID,Name,age,Occupation,Salary) values(01,'Alex',34,'Admin',50000),(02,'lily',37,'Marketing',45000),(03,'john',30,'Database',40000),(04,'William',40,'Manager',60000)
+
+-- select * from AnnualSalary
+
+-- update AnnualSalary set Salary = 10000 where name = 'john'
+-- annualsalary
+-- create table Students(Id int, Name varchar(20), Age int, Subject varchar(15), Mark int)
+
+-- insert into Students(Id,Name,Age,Subject,Mark) values(01,'Fathima',15,'Maths',50),(02,'Naseem',15,'Maths',36),(03,'Nasrin',16,'Maths',60),(04,'Emil',15,'Maths',45),(05,'George',16,'Maths',56)
+
+-- create table  grp1(name varchar(10),salary int)
+-- insert into grp1(name,salary) values('er',1480),('rt',230)
+-- select * from grp1
+-- ALTER table Students ADD primary key (Id)
